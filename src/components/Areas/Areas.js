cfdae793b3044d9fd2b78f03c2fd11eef1274fec
Daniel Orio <danielislas3@live.com>
2019-09-23 17:24:28 -0500

    Obtener props de Servicio y Zona. Mostrarlas en Areas.js

--- src/components/Areas/Areas.js
+++ src/components/Areas/Areas.js
@@ -5,16 +5,34 @@ import { Descriptions } from 'antd';
 const { Panel } = Collapse;
 const ButtonGroup = Button.Group;
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
-
 
 export default class Areas extends Component {
 
   state = {
+     data:[
+       {
+         id:999,
+         name:'A',
+         count:421,
+         states:['CDMX','Aguascalientes', 'Baja', 'California', 'Mexicali', 'Baja', 'California', 'Sur', 'La Paz', 'Campeche', 'San' ,'Francisco' ,'de', 'Campeche','Chihuahua', 'Chihuahua', 'Tuxtla' ,'Gutiérrez','Coahuila', 'Saltillo', 'Colima', 'Colima','Francisco' ,'de', 'Campeche','Chihuahua', 'Chihuahua', 'Tuxtla' ,'Gutiérrez','Coahuila', 'Saltillo', 'Colima', 'Colima'],
+
+       },
+       {
+         id:890,
+         name:'B',
+         count:321,
+         states:['Francisco' ,'de', 'Campeche','Chihuahua', 'Chihuahua', 'Tuxtla' ,'Gutiérrez','Coahuila', 'Saltillo', 'Colima', 'Colima'],
+
+       },
+       {
+         id:234,
+         name:'C',
+         count:543,
+         states:['CDMX','Aguascalientes', 'Baja', 'California', 'Mexicali', 'Baja', 'California', 'Sur', 'La Paz', 'Campeche', 'Chihuahua', 'Tuxtla' ,'Gutiérrez','Coahuila', 'Saltillo', 'Colima', 'Colima'],
+
+       },
+      ],
+    currentService: 'EXPRESS DOMESTIC',
     count: 5,
     show: true,
   }
@@ -41,59 +59,36 @@ export default class Areas extends Component {
 
   render() {
     return (
-      <div>
+      <>
         <Link to="/">
           Home
         </Link>
         <h1>Areas</h1>
-        <div>
-          <Badge count={this.state.count}>
-            <a href="#" className="head-example" />
-          </Badge>
-
-          <ButtonGroup>
-            <Button onClick={this.decline}>
-              <Icon type="minus" />
-            </Button>
-            <Button onClick={this.increase}>
-              <Icon type="plus" />
-            </Button>
-          </ButtonGroup>
-        </div>
-       
-        <div>
-          <Collapse onChange={this.toggleCollapse}>
-            <Panel header="EXPRESS DOMESTIC" key="1">
-              <Collapse defaultActiveKey="1">
-                <Panel header="Area" key="1">
-                    <p>Centro</p>
-                    <p>15 CP</p>
-                    <p>CDMX</p>
-                </Panel>
-              </Collapse>
-
-              <Collapse defaultActiveKey="1">
-                <Panel header="Area B" key="1">
-
-                  <Descriptions title="kanguro envio de 1 a 3 dias" layout="vertical"
-                    column={{ xxl: 4, xl: 3, lg: 2, md: 2, sm: 2, xs: 1 }}>
-                    <Descriptions.Item label="Estados">Hangzhou, Zhejiang,Hangzhou, Zhejiang,Hangzhou, Zhejiang,Hangzhou, Zhejiang,</Descriptions.Item>
-                    <Descriptions.Item label="Codigo postales">434</Descriptions.Item>
-                  </Descriptions>
-
-                </Panel>
-              </Collapse>
-
-
+        <h3>{this.state.currentService}</h3>
+        
+              {this.state.data.map(area =>
+                <>
+                <Collapse defaultActiveKey="0">
+                  <Panel header={area.name} key="1">
+  
+                    <Descriptions title="kanguro envio de 1 a 3 dias" layout="vertical"
+                      column={{ xxl: 4, xl: 3, lg: 2, md: 2, sm: 2, xs: 1 }}>
+                      <Descriptions.Item label="Estados">Hangzhou, Zhejiang,Hangzhou, Zhejiang,Hangzhou, Zhejiang,Hangzhou, Zhejiang,</Descriptions.Item>
+                      <Descriptions.Item label="Codigo postales">434</Descriptions.Item>
+                    </Descriptions>
+  
+                  </Panel>
+                </Collapse>
+                </>
+              )}
               <div>
                 <h2>Tarifas</h2>
               </div>
-            </Panel>
+        </>
+   
+  
 
-          </Collapse>
-        </div>
-
-      </div>
+   
     )
   }
 }
